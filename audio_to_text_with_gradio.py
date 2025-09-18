d06170563bc20b1003fea7df5836643b55f1c7a2
williangama <wgama@kitmanalabs.com>
2025-09-18 16:51:04 +0100

    rename vars

--- audio_to_text_with_gradio.py
+++ audio_to_text_with_gradio.py
@@ -1,5 +1,5 @@
 import gradio
-import soundfile as sf
+import soundfile
 import librosa
 from nemo.collections.asr.models import ASRModel
 import os
@@ -7,9 +7,9 @@ import os
 model = ASRModel.from_pretrained(model_name="nvidia/canary-1b-v2")
 
 def convert_audio_stereo_to_mono(audio_path):
-    y, sr = librosa.load(audio_path, sr=model.cfg.preprocessor.sample_rate, mono=True)
+    audi_data, sample_rate = librosa.load(audio_path, sr=model.cfg.preprocessor.sample_rate, mono=True)
     temp_file = "temp.wav"
-    sf.write(temp_file, y, sr)
+    soundfile.write(temp_file, audi_data, sample_rate)
     return temp_file
 
 def transcribe(audio_file):
