51111ece2189e0d769c338248a5cd7bd1926a415
williangama <wgama@kitmanalabs.com>
2025-09-18 16:57:17 +0100

    add old code for testing purposes

--- translation.py
+++ translation.py
@@ -1,3 +1,4 @@
+Transformers: https://huggingface.co/docs/transformers/main_classes/pipelines
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 
 # Load model and tokenizer
@@ -18,3 +19,22 @@ translated_text = tokenizer.decode(translated_tokens[0], skip_special_tokens=Tru
 
 print("Original:", "Hello, how are you today?")
 print("Translated:", translated_text)
+
+# Old
+# from transformers import pipeline
+#
+# translator = pipeline(
+#     task="translation",
+#     model="facebook/m2m100_1.2B"
+# )
+#
+# text_to_translate = "Dinner is ready"
+# result = translator(
+#     text_to_translate,
+#     src_lang="en",
+#     tgt_lang="pt",
+#     clean_up_tokenization_spaces=True
+# )
+#
+# print("Original:", text_to_translate)
+# print("Translated:", result[0]['translation_text'])
